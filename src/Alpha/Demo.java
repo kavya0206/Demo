e6a1c1ae5a3b79acaf7075de3ba7e24381942ae2
Kavya <kavyact02@gmail.com>
2021-10-19 19:32:00 +0530

    first commit

---
+++ src/Alpha/Demo.java
@@ -0,0 +1,20 @@
+package Alpha;
+import org.apache.logging.log4j.*;
+
+public class Demo {
+
+	//entire path of the class - Demo.class.getName()
+	private static Logger log = LogManager.getLogger(Demo.class.getName()); 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log.debug("I have clicked on button");
+			log.info("button is diplayed");
+		log.error("Button is not clicked");
+		
+		log.fatal("button is missing");
+
+	}
+
+}
---
+++ src/Beta/Demo1.java
@@ -0,0 +1,21 @@
+package Beta;
+import org.apache.logging.log4j.*;
+
+public class Demo1 {
+
+	//entire path of the class - Demo1.class.getName()
+	private static Logger log = LogManager.getLogger(Demo1.class.getName()); 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log.debug("I am debugging");
+		if(5>4)
+			log.info("object is present");
+		log.error("Object is not present");
+		
+		log.fatal("Fatal");
+
+	}
+
+}
